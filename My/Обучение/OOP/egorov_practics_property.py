c36fae5551248aadcd8580ccca207634c7d8b15b
koskin17 <kostia.zeykin@gmail.com>
2022-11-16 11:55:48 +0200

    Property practice

--- My/Обучение/OOP/egorov_practics_property.py
+++ My/Обучение/OOP/egorov_practics_property.py
@@ -1,10 +1,48 @@
 # Практика по property
 
+from string import digits
+
+
 class User:
 
     def __init__(self, login, password):
         self.login = login
-        self.password = password
+        self.__password = password
+
+    @property
+    # По умолчанию эта функция getter
+    def password(self):
+        print("Getter called")
+        return self.__password
+
+    # Проверка пароля на наличие цифр.
+    # Для того, чтобы self не принимался, вешаем декоратор staticmethod
+    @staticmethod
+    def is_include_number(password):
+        # Для проверки используем метод digits из модуля string
+        for digit in digits:
+            if digit in password:
+                return True
+
+            return False
+
+    # Устанавливает свойству метод setter
+    @password.setter
+    def password(self, value):
+        # В setter добавляет проверку и вызываем исключение
+        if not isinstance(value, str):
+            raise TypeError("Пароль должен быть строкой.")
+        if len(value) < 4:
+            raise ValueError("Пароль слишком короткий. Минимум 3 символа.")
+        if len(value) > 12:
+            raise ValueError("Пароль слишком длинный. Максимум 12 символов.")
+        # Для проверки пароля на наличие цифр создадим отдельную функцию
+        # Перед установкой пароля вызываем метод проверки для пароля
+        if not User.is_include_number(value):
+            raise ValueError("Пароль должен содержать цифры")
+
+        print("Setter called")
+        self.__password = value
 
 
 """Проблема, которая возникает при работе с классом в том, что можно спокойно обращаться к аттрибутам этого
@@ -14,6 +52,13 @@ class User:
 - пароль всегда должен быть строкой;
 - нужно ззакрыть доступ к паролю;
 - мы должны быть уверены, что пароль безопасный.
-Для всего этого и нужно property. С его помощью можно отловить момент, когда"""
+Для всего этого и нужно property. С его помощью можно отловить момент, когда к аттрибуту идёт обращение или
+попытка его изменить.
+Т.е. property отлавливает событие getter и событе setter.
+Соответственно, при отлавливании события на него можно повлиять и, к примеру, запретить."""
 
-user1 = User("Ivan", 123)
+user1 = User(input("Введите логин: "), input("Введите пароль: "))
+print(user1.password)
+
+user1.password = input("Введите новый пароль: ")
+print("Новый пароль: ", user1.password)

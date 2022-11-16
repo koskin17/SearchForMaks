6ee5ca0f299df4391fbd53bf21dd57ae380b8721
koskin17 <kostia.zeykin@gmail.com>
2022-11-16 10:44:24 +0200

    Property practice

---
+++ My/Обучение/OOP/egorov_practics_property.py
@@ -0,0 +1,8 @@
+# Практика по property
+class User:
+
+    def __int__(self, login, password):
+        self.login = login
+        self.password = password
+
+# Проблема, которая возникает при работе с классом

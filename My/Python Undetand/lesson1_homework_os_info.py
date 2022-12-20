3173c1c2fd1c89e9b4d7475f80f96df9ca7aca59
koskin17 <kostia.zeykin@gmail.com>
2022-12-20 12:00:34 +0200

    change string with information about system to f-string

--- My/Python Undetand/lesson1_homework_os_info.py
+++ My/Python Undetand/lesson1_homework_os_info.py
@@ -1,10 +1,9 @@
 # Нужно собрать информацию про операционную систему и версию Python
-# TODO запустить этот скрипт и закомитить результат работы (файд os_info.txt)
+
 import platform
 import sys
 
-info = 'OS info is \n{}\n\nPython version is {}{}'.format(
-    platform.uname(), sys.version, platform.architecture())
+info = f"OS info is \n{platform.uname()}\n\nPython version is {sys.version}{platform.architecture()}"
 
 print(info)
 with open('os_info.txt', 'w') as ff:

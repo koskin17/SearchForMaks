bba624071e9613bd81e361007b88353c0e14f915
koskin <118272123+koskin17@users.noreply.github.com>
2024-09-24 06:43:26 +0300

    Try 1

---
+++ SearchForMaks/test.py
@@ -0,0 +1,44 @@
+import py7zr
+from os import walk
+from os import remove
+from os import path
+
+
+
+pattern = 'J10807ES'
+files_with_pattern = []
+
+# path = r"E:\Python projects\SearchForMaks\tmp" # используйте нужный метод задания ссылки на папку
+
+# for root, dirs, files in walk(path):
+#     for file in files:
+#         if pattern in file:
+#             files_with_pattern.append(file)
+        
+# print(files_with_pattern)
+
+with py7zr.SevenZipFile(r"E:\Python projects\SearchForMaks\Оригиналы\Delta_5.edz", 'r') as archive:
+    """Getting list of all files in folder"""
+    all_files = archive.getnames()
+
+    for file in all_files:
+        if pattern in file:
+            folder_with_nessessary_files=r"E:\Python projects\SearchForMaks\tmp"
+            archive.extractall(folder_with_nessessary_files)
+            
+            for root, dirs, files in walk(folder_with_nessessary_files):
+                for file in files:
+                    print(f"{root}\\{file}")
+            #         if pattern in file:
+            #             files_with_pattern.append(file)
+            
+            # for root, dirs, files in walk(folder_with_nessessary_files):
+            #     for file in files:
+            #         if file not in ".".join(files_with_pattern) and not file.endswith(".pdf") and not (file.endswith(".jpg") or file.endswith(".JPG")):
+            #             remove(f"{root}{file}")
+
+                                
+            # print(files_with_pattern)
+            # print(pattern in ".".join(files_with_pattern))
+            
+            break

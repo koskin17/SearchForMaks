275172be9d4ef25bde53da9d7f14805682f6800a
koskin <118272123+koskin17@users.noreply.github.com>
2024-09-24 11:34:57 +0300

    Fixed minor errors

--- SearchForMaks/test.py
+++ SearchForMaks/test.py
@@ -10,23 +10,23 @@ with py7zr.SevenZipFile(r"E:\Python projects\SearchForMaks\Оригиналы\De
     """Getting list of all files in folder"""
     all_files = archive.getnames()
 
-    """Making list of files with nessesary text"""
+    """Making list of files with necessary text"""
     for file in all_files:
         if pattern in file:
-            folder_with_nessessary_files=r"E:\Python projects\SearchForMaks\tmp"
-            archive.extractall(folder_with_nessessary_files)
+            folder_with_necessary_files=r"E:\Python projects\SearchForMaks\tmp"
+            archive.extractall(folder_with_necessary_files)
             
-            for root, dirs, files in walk(folder_with_nessessary_files):
+            for root, dirs, files in walk(folder_with_necessary_files):
                 for file in files:
                     print(f"{root}\\{file}")
                     if pattern in file:
                         files_with_pattern.append(file)
             
-            """Deleting unnessesary files"""
-            for root, dirs, files in walk(folder_with_nessessary_files):
+            """Deleting unnecessary files"""
+            for root, dirs, files in walk(folder_with_necessary_files):
                 for file in files:
                     if file not in ".".join(files_with_pattern) and not file.endswith(".pdf") and not (file.endswith(".jpg") or file.endswith(".JPG")):
                         remove(f"{root}\\{file}")
             
-            """As a result we get all folders from original archive with nessesary files"""
+            """As a result we get all folders from original archive with necessary files"""
             break
